18af599e6c4ff791a877376f5b291ad186756523
Melody Blachowicz <melodyblachowicz@gmail.com>
2021-08-09 13:43:20 -0700

    comment addition

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,3 +1,21 @@
+// Here is an example return from Open Weather Map API:
+
+// Object
+// base: "stations"
+// clouds: {all: 75}
+// cod: 200
+// coord: {lon: -112.074, lat: 33.4484}
+// dt: 1628541360
+// id: 5308655
+// main: {temp: 90.12, feels_like: 94.82, temp_min: 84.24, temp_max: 96.89, pressure: 1011, …}
+// name: "Phoenix"
+// sys: {type: 2, id: 2008537, country: "US", sunrise: 1628513206, sunset: 1628562050}
+// timezone: -25200
+// visibility: 10000
+// weather: [{…}]
+// wind: {speed: 4, deg: 38, gust: 8.99}
+
+
 // Create Weather class
 class Weather {
   constructor(city, state) {
